9d0be53c7556c9661c0e88bc508f104763211cf8
xfinity_ai <collinslangat4@gmail.com>
2023-07-20 03:00:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints with puts
+ * Return: 0 (always success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
