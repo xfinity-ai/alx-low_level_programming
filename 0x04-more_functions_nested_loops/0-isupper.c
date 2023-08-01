bb43539b96d7b8d290b7ce539bf9da471cd9f4ff
xfinity_ai <collinslangat4@gmail.com>
2023-08-01 13:26:30 -0700

    lupu mob

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - Checks for uppercase characters
- * @c: The character to be checked
- * 
- * Return: 1 if character is uppercase, 0 otherwise
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-
 	else
 		return (0);
 }
+
